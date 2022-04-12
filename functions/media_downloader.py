76ac27b5cab297d142cb00e14dda815755cec5df
Gabey <fluffygabey@gmail.com>
2022-04-12 11:19:27 +0200

    structured the media rooting

--- functions/media_downloader.py
+++ functions/media_downloader.py
@@ -1,4 +1,4 @@
-# MEDIA RELATED FUNCTIONS
+# DOWNLOADING MEDIAS
 # ------------------------------------
 # download an image from an url.
 def download_image(link, media_folder):
@@ -9,6 +9,47 @@ def download_multiple_image(links, media_folder):
 # download 
 def download_video(video, audio, media_folder):
     pass
+
+# FETCHING LINKS
+# ------------------------------------
+# explore reddit gallery and return links to media
+def galery_explorer(sumission, max=4):
+    pass
+# explore reddit video and return links to the right stream.
+def video_explorer(submission, max=512):
+    # 512 MB masx
+    pass
+
+# ROOTING
+# ------------------------------------
 # returns links to the media.
 def media_rooter(submission):
-    pass
+    submission_url = submission['url']
+    
+    if 'v.redd.it' in submission_url:
+        return {
+            'type': 'video',
+            'audio': 'tbd',
+            'video': video_explorer(submission)
+        }
+    elif 'i.redd.it' in submission_url:
+        return {
+            'type': 'image',
+            'link': submission_url
+        }
+    elif 'i.imgur.com' in submission_url:
+        return {
+            'type': 'imgure',
+            'link': submission_url
+        }
+    elif 'gallery' in submission_url:
+        return {
+            'type': 'gallery',
+            'links': galery_explorer(submission)
+        }
+    else:
+        return {
+            'type': 'unsupported',
+            'error': f"{submission_url} is unsupported"
+        }
+    
