0a8aa2d67c01d89c5291e93e36ce5fae8697d2fd
Gabey <fluffygabey@gmail.com>
2022-04-16 16:45:04 +0200

    update argument for build_tweet function

--- functions/twitter_api.py
+++ functions/twitter_api.py
@@ -8,7 +8,6 @@ def log_on_twitter_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     return tweepy.API(auth, wait_on_rate_limit=True)
-
 # verify that the twitter api credentials are up to date.
 def verify_twitter_credentials(api):
     try:
@@ -18,12 +17,12 @@ def verify_twitter_credentials(api):
         print("Error during authentication")
         return False
     return True
-# build the tweet as a dictionarry.
-def build_tweet():
+# return a valid tweet
+def build_tweet(body, media_folder):   
     pass
 # send the tweet once built
 def send_tweet(api, tweet):
     media = api.media_upload(tweet.path_picture)
-    post_result = api.update_status(status=tweet.body, media_ids=[media.media_id])
+    post_result = api.update_status(status=tweet['body'], media_ids=[media.media_id])
 
     print('We successfully tweeted')
