21091f19d0e6c653d03ad025ea778e1c9c98d790
Gabey <fluffygabey@gmail.com>
2022-04-16 20:27:33 +0200

    main is done
    I still have to implement functions though the script should be okay.

--- main.py
+++ main.py
@@ -7,31 +7,41 @@ from creds import *
 
 # ------------------------------------
 
-# This is queue. We only fetch top posts hence why we will only save the last 400 ids.
-previous_post_list = [0] * 400 
+def tweet_first(submissions, media_folder, tweet_body):
+    for submission in submissions:
+        empty_folder(media_folder)
+        
+        metadata = media_rooter(submission)
+        media_downloader(metadata, media_folder)
 
-
-reddit_api = log_on_reddit_api(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT)
+        tweet = build_tweet(tweet_body, media_folder)
+        send_tweet(twitter_api, tweet)
 
 # ------------------------------------
 
-# maximum possible index
-i_max = len(subreddit_list) - 1
+# This is queue. We only fetch top posts hence why we will only save the last 400 ids.
+previous_post_list = [0] * 400 
 
-# 542 % 5 = 2...  543 % 5 = 3...  544 % 5 = 4...  545 % 5 = 0
+reddit_api = log_on_reddit_api(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT)
+twitter_api = log_on_twitter_api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+if not reddit_api or not twitter_api:
+    exit('Could not connect to APIs.')
+
+# ------------------------------------
+
+i_max = len(subreddit_list) - 1 # maximum possible index
+
 # using the modulo provides the index a natural rotation.
-try:
-    current_i = fetch_state(fSTATE) % (i_max)
+try: current_i = fetch_state(fSTATE) % (i_max)
 except (FileNotFoundError, TypeError):
     create_state_file(fSTATE)   
     current_i = 0
 
 subreddit_name = subreddit_list[current_i][0]
 subreddit = reddit_api.subreddit(subreddit_name)
+submissions = fetch_submission(subreddit, previous_post_list, banned_post_list, 3)
 
-
-# program
-# program
-# program
+tweet_first(submissions, fMEDIA_FOLDER, subreddit_list[fSTATE])
 
 increment_state(fSTATE)
