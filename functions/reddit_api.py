832a43431be21f87831de0b37a8fe444fd0747b1
Gabey <fluffygabey@gmail.com>
2022-04-16 17:38:04 +0200

    added basic exception handling.

--- functions/reddit_api.py
+++ functions/reddit_api.py
@@ -1,28 +1,35 @@
 import praw
+from prawcore.exceptions import PrawcoreException
 
 # REDDIT RELATED FUNCTIONS
 # ------------------------------------
 # returns the api object after identifying to the reddit api.
 def log_on_reddit_api(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET, REDDIT_USER_AGENT):
+    reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,client_secret=REDDIT_CLIENT_SECRET,user_agent=REDDIT_USER_AGENT)
     try:
-        reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,client_secret=REDDIT_CLIENT_SECRET,user_agent=REDDIT_USER_AGENT)
+        test_connection(reddit)
         print("Authentication to reddit OK")
-    except:
+    except PrawcoreException:
         print("Error during authentication to reddit")
         return False
     return reddit
 # returns a list of submission from the a subreddit.
 def fetch_submission(subreddit, previous_post_list, banned_post_list, limit=10):
     posts = []
-    
-    for submission in  subreddit.hot(limit=limit):
-        if submission not in (previous_post_list or banned_post_list):
-            posts.append(
-                {
-                   'id': submission.id,
-                   'url': submission.url,
-                   'permalink': submission.permalink
-                }
-            )
+    try:
+        for submission in  subreddit.hot(limit=limit):
+            if submission not in (previous_post_list or banned_post_list):
+                posts.append(
+                    {
+                    'id': submission.id,
+                    'url': submission.url,
+                    'permalink': submission.permalink
+                    }
+                )
+    except PrawcoreException as e:
+        print(e)
     
     return posts
+def test_connection(reddit_api):
+    submission = reddit_api.submission(id="39zje0").title
+    
