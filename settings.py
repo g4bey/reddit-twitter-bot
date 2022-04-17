f21255e63c3c0fa42e937baf4c089b805dfd6667
Gabey <fluffygabey@gmail.com>
2022-04-17 21:13:21 +0200

    Write doctring for setting

--- settings.py
+++ settings.py
@@ -1,3 +1,25 @@
+"""Settings for the application.
+
+fSTATE: the state of your program.
+    / it helps alternating between subreddits
+
+fPREVIOUS_POSTS: store the 400 previous posts.
+
+fMEDIA_FOLDER: home of the downloaded medias.
+
+banned_post_list: posts you want to ban.
+    / often subreddit have pinned posts.
+    / this won't affect the application
+    / but that's a waste of ressource.
+
+submission_to_fetch: submission you want to fetch.
+    / Media will be indexed, not downloaded.
+    / the script stops at the first
+    / tweet posted.
+
+subreddit_list: the list of subreddit to alternate with.
+"""
+
 fSTATE = ".program_state"
 fPREVIOUS_POSTS = ".previous_posts"
 fMEDIA_FOLDER = "media"
