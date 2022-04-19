a2d786e74823f56f6703d210e1e6a80aa774e1e2
Gabey <fluffygabey@gmail.com>
2022-04-19 14:56:17 +0200

    create api_connector

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 """Publish content from reddit to twitter."""
 
 import logging
-import settings as conf
 from random import choice
-from scripts import init_logger
+import creds
+from scripts import *
+import settings as conf
+
+
 
 # initialiation logging.
 init_logger(conf.logger)
+
+#log on the apis.
+# read the documentation for dict keys.
+log_on_twitter_api(**creds.twitter)
+log_on_reddit_api(**creds.reddit)
 
 
 # picks a random subreddit.
--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,3 +1,5 @@
 """Bundle scripts into a package."""
 
 from .logger import init_logger
+from .api_connector import log_on_twitter_api
+from .api_connector import log_on_reddit_api
